75f3e5cca80078318906fa7b537814e8bc31d3e8
babucarr32 <baboucarry@gmail.com>
2022-05-19 22:06:51 +0000

    Drop in me any amount of text with URLS in it, I will hanlde them for you.

---
+++ link-opener.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+text = open(r"C:\Users\USER\Documents\Program\Dump\links\Qcell", "r")
+text.read()
+
+for link in text:
+	web  = webbrowser.open("https://pr0gramm.com/user/Danso")
